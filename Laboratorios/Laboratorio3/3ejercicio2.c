503a20b70e2fa36b9f211d7f3e6caf4fe9c8d505
Lincoln <davidveganaranjo08@gmail.com>
2025-06-15 23:44:11 -0600

    Implemento codigo

---
+++ Laboratorios/Laboratorio3/3ejercicio2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int encontrarMaximo (int arr[], int n) {
+    int maximo = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maximo) {
+            maximo = arr[i];
+        }
+    }
+
+    return maximo;
+}
+
+int main() {
+    int numeros[ ] = {10, 20, 5, 40, 30};
+    int n = sizeof(numeros) / sizeof(numeros[0]);
+    printf("Array: \n");
+    for (int e = 0; e < n; e++) {
+        printf("%d ", numeros[e]);
+    }
+    printf("\n");
+    int maximo = encontrarMaximo (numeros, n);
+    printf("El numero mas grande es: %d\n", maximo);
+    return 0;
+}
